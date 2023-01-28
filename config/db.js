9c4c5d611ef5056ec35aa22dc260e7bbfe486298
bigmanabel <admin@wewritetech.com>
2023-01-28 12:34:20 +0000

    got to homepage

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host:'localhost',
     user: 'nigesppx_survey',
-    password: 'BqqatZfpRwTi',
-    database: 'nigesppx_survey' 
+    password:'BqqatZfpRwTi',
+    database:'nigesppx_survey' 
 });
 
 module.exports = pool.promise();
